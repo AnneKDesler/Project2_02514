7cac1d7a451bc40916de9ef4be85f36e546c1cb1
Anne Kjerstine Desler <s194333@dtu.dk>
2023-06-09 09:24:56 +0200

    Just copied the files from project1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Project 1 at the Deep Learning in Computer Vision course at DTU",
+    author="Group20",
+    license="MIT",
+)
