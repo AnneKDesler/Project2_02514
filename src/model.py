f595913e0d6a14e65c7bc872b06a42dd7d3dbd43
luchsonice <s194369@student.dtu.dk>
2023-06-12 09:57:59 +0200

    n

--- src/model.py
+++ src/model.py
@@ -3,97 +3,68 @@ import pytorch_lightning as pl
 import torch
 from torch import nn
 from torch.nn import functional as F
-import numpy as np
 
 
 
 class Model(pl.LightningModule):
     def __init__(
         self,
-        num_classes: int = 1,
+        num_classes: int = 2,
         lr: Optional[float] = 1e-3,
         weight_decay: Optional[float] = 0,
         batch_size: Optional[int] = 1,
         batch_normalization: Optional[bool] = False,
         optimizer: Optional[str] = None,
-        target_mask_supplied: Optional[bool]=False,
         *args,
         **kwargs
     ) -> None:
         super(Model, self).__init__(*args, **kwargs)
-        
-        self.target_mask_supplied = target_mask_supplied
 
         # encoder (downsampling)
         self.enc_conv0 = nn.Sequential(nn.Conv2d(3, 64, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(64, 64, 3, stride=1, padding=1),
-                                       nn.ReLU())
-        
+                                       nn.Conv2d(64, 64, 3, stride=1, padding=1))
         self.downsample0 = nn.Conv2d(64, 64, 3, stride=2, padding=1)
 
         self.enc_conv1 = nn.Sequential(nn.Conv2d(64, 128, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(128, 128, 3, stride=1, padding=1),
-                                       nn.ReLU())
+                                       nn.Conv2d(128, 128, 3, stride=1, padding=1))
         self.downsample1 = nn.Conv2d(128, 128, 3, stride=2, padding=1)
         
         self.enc_conv2 = nn.Sequential(nn.Conv2d(128, 256, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(256, 256, 3, stride=1, padding=1),
-                                       nn.ReLU())
-        
+                                       nn.Conv2d(256, 256, 3, stride=1, padding=1))
         self.downsample2 = nn.Conv2d(256, 256, 3, stride=2, padding=1)
         
         self.enc_conv3 = nn.Sequential(nn.Conv2d(256, 512, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(512, 512, 3, stride=1, padding=1),
-                                       nn.ReLU())
-        
+                                       nn.Conv2d(512, 512, 3, stride=1, padding=1))
         self.downsample3 = nn.Conv2d(512, 512, 3, stride=2, padding=1)
 
         # bottleneck
         self.bottleneck_conv = nn.Sequential(nn.Conv2d(512, 1024, 3, stride=1, padding=1),
-                                             nn.ReLU(),
-                                             nn.Conv2d(1024, 1024, 3, stride=1, padding=1),
-                                             nn.ReLU())
+                                             nn.Conv2d(1024, 1024, 3, stride=1, padding=1))
 
         # decoder (upsampling)
         self.upsample0 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
         
         self.dec_conv0 = nn.Sequential(nn.Conv2d(1024, 512, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(512, 512, 3, stride=1, padding=1),
-                                       nn.ReLU())
+                                       nn.Conv2d(512, 512, 3, stride=1, padding=1))
         
         self.upsample1 = nn.ConvTranspose2d(512, 256, 2, stride=2)
         
         self.dec_conv1 = nn.Sequential(nn.Conv2d(512, 256, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(256, 256, 3, stride=1, padding=1),
-                                       nn.ReLU())
+                                       nn.Conv2d(256, 256, 3, stride=1, padding=1))
         
         self.upsample2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
         
         self.dec_conv2 = nn.Sequential(nn.Conv2d(256, 128, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(128, 128, 3, stride=1, padding=1),
-                                       nn.ReLU())
+                                       nn.Conv2d(128, 128, 3, stride=1, padding=1))
         
         self.upsample3 = nn.ConvTranspose2d(128, 64, 2, stride=2)
         
         self.dec_conv3 = nn.Sequential(nn.Conv2d(128, 64, 3, stride=1, padding=1),
-                                       nn.ReLU(),
                                        nn.Conv2d(64, 64, 3, stride=1, padding=1),
-                                       nn.ReLU(),
-                                       nn.Conv2d(64, num_classes, 1, stride=1))
+                                       nn.Conv2d(64, num_classes, 3, stride=1, padding=1))
         self.lr = lr
         self.batch_size = batch_size
-        if num_classes == 1:
-            self.loss = torch.nn.BCEWithLogitsLoss()
-        else:
-            self.loss = torch.nn.CrossEntropyLoss()
-
+        self.loss = torch.nn.CrossEntropyLoss()
         if optimizer is None or optimizer == "Adam":
             self.optimizer = torch.optim.Adam(
                 self.parameters(), lr=self.lr, weight_decay=weight_decay
@@ -107,37 +78,36 @@ class Model(pl.LightningModule):
         """
 
         # encoder
-        e0 = self.enc_conv0(x)
+        e0 = F.relu(self.enc_conv0(x))
         e0_down = self.downsample0(e0)
-        e1 = self.enc_conv1(e0_down)
+        e1 = F.relu(self.enc_conv1(e0_down))
         e1_down = self.downsample1(e1)
-        e2 = self.enc_conv2(e1_down)
+        e2 = F.relu(self.enc_conv2(e1_down))
         e2_down = self.downsample2(e2)
-        e3 = self.enc_conv3(e2_down)
+        e3 = F.relu(self.enc_conv3(e2_down))
         e3_down = self.downsample3(e3)
 
         # bottleneck
-        b = self.bottleneck_conv(e3_down)
+        b = F.relu(self.bottleneck_conv(e3_down))
         # decoder
         b = self.upsample0(b)
         b = torch.cat((b, e3), dim=1)  # skip-connection
-        d0 = self.dec_conv0(b)
+        d0 = F.relu(self.dec_conv0(b))
 
         d0 = self.upsample1(d0)
 
         d0 = torch.cat((d0, e2), dim=1)  # skip-connection
-        d1 = self.dec_conv1(d0)
+        d1 = F.relu(self.dec_conv1(d0))
 
         d1 = self.upsample2(d1)
 
         d1 = torch.cat((d1, e1), dim=1)  # skip-connection
-        d2 = self.dec_conv2(d1)
+        d2 = F.relu(self.dec_conv2(d1))
 
         d2 = self.upsample3(d2)
 
         d2 = torch.cat((d2, e0), dim=1)  # skip-connection
         d3 = self.dec_conv3(d2)  # no activation
-        
         return d3
 
     def _inference_training(
@@ -146,20 +116,13 @@ class Model(pl.LightningModule):
         """
         From https://huggingface.co/docs/transformers/model_doc/t5#training
         """
-        if self.target_mask_supplied:
-            data, target, mask = batch
-        else:
-            data, target = batch
+
+        data, target = batch
         output = self(data)
-        if self.target_mask_supplied:
-            output *= mask[:,None,:,:]
-
         output, target = output.flatten(), target.flatten()
-        
+        preds = torch.round(output)
+        _,_,accuracy,_,_ = self.metrics(preds, target)
         target = target.to(torch.float32)
-
-        _,_,accuracy,_,_ = self.metrics(output, target)
-
 
         return self.loss(output, target), accuracy
 
@@ -193,14 +156,8 @@ class Model(pl.LightningModule):
 
     def metrics(self, preds, target):
         # Dice
-        X = target#/255
-        print(torch.sigmoid(preds).max())
-        print(torch.sigmoid(preds).min())
+        X = target
         Y = torch.sigmoid(preds) > 0.5
-
-        Y = Y*1.0
-        print(X.unique())
-        print(Y.unique())
         dice = 2*torch.mean(torch.mul(X,Y))/torch.mean(X+Y)
 
         # Intersection over Union
@@ -210,7 +167,6 @@ class Model(pl.LightningModule):
         accuracy =  torch.logical_and(Y, X).sum() / len(X)
 
         TP = torch.logical_and(Y, X).sum() #(preds == target == 1).sum()
-        print(TP)
         FP = torch.logical_not(torch.logical_and(torch.logical_not(Y), X)).sum()# (preds != target == 0).sum()
         TN = torch.logical_not(torch.logical_and(Y, X)).sum() # (preds == target == 0).sum()
         FN = torch.logical_and(torch.logical_not(Y), X).sum() # (preds != target == 1).sum()
@@ -260,11 +216,11 @@ class DilatedNet(pl.LightningModule):
         self.dec_conv0 = nn.ConvTranspose2d(64, 64, 3, dilation=8)
         self.dec_conv1 = nn.ConvTranspose2d(64, 64, 3, dilation=4)
         self.dec_conv2 = nn.ConvTranspose2d(64, 64, 3, dilation=2)
-        self.dec_conv2 = nn.ConvTranspose2d(64, 1, 3, dilation=1)
+        self.dec_conv3 = nn.ConvTranspose2d(64, 1, 3, dilation=1)
 
         self.lr = lr
         self.batch_size = batch_size
-        self.loss = torch.nn.CrossEntropyLoss()
+        self.loss = torch.nn.BCEWithLogitsLoss()
         if optimizer is None or optimizer == "Adam":
             self.optimizer = torch.optim.Adam(
                 self.parameters(), lr=self.lr, weight_decay=weight_decay
@@ -277,7 +233,7 @@ class DilatedNet(pl.LightningModule):
         e0 = F.relu(self.enc_conv0(x))
         e1 = F.relu(self.enc_conv1(e0))
         e2 = F.relu(self.enc_conv2(e1))
-        e3 = F.relu(self.enc_conv2(e2))
+        e3 = F.relu(self.enc_conv3(e2))
 
         # bottleneck
         b = F.relu(self.bottleneck_conv(e3))
@@ -285,8 +241,8 @@ class DilatedNet(pl.LightningModule):
         # decoder
         d0 = F.relu(self.dec_conv0(b))
         d1 = F.relu(self.dec_conv1(d0))
-        d2 = F.relu(self.dec_conv1(d1))
-        d3 = F.softmax(self.dec_conv2(d2))
+        d2 = F.relu(self.dec_conv2(d1))
+        d3 = self.dec_conv3(d2)
         return d3
 
     def _inference_training(
@@ -298,11 +254,10 @@ class DilatedNet(pl.LightningModule):
 
         data, target = batch
         output = self(data)
-        output, target = output.flatten(), target.flatten()
-        preds = torch.round(output)
-        _,_,accuracy,_,_ = self.metrics(preds, target)
         target = target.to(torch.float32)
-
+        target = target/255
+        preds = output
+        _,_,accuracy,_,_ = self.metrics(preds, target)
         return self.loss(output, target), accuracy
 
     def training_step(
@@ -327,7 +282,6 @@ class DilatedNet(pl.LightningModule):
         loss, accuracy = self._inference_training(batch, batch_idx)
         self.log("test loss", loss, batch_size=self.batch_size, sync_dist=True)
         self.log("test accuracy", accuracy, batch_size=self.batch_size, sync_dist=True)
-
         return loss
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
@@ -335,7 +289,7 @@ class DilatedNet(pl.LightningModule):
 
     def metrics(self, preds, target):
         # Dice
-        X = (target-torch.mean(target))/torch.std(target) > 0.5
+        X = target
         Y = torch.sigmoid(preds) > 0.5
         dice = 2*torch.mean(torch.mul(X,Y))/torch.mean(X+Y)
 
@@ -343,12 +297,12 @@ class DilatedNet(pl.LightningModule):
         IoU = torch.mean(torch.mul(X,Y))/(torch.mean(X+Y)-torch.mean(torch.mul(X,Y)))
 
         # Accuracy
-        accuracy =  (preds == target).sum() / len(target)
+        accuracy =  torch.logical_and(Y, X).sum() / len(X)
 
-        TP = (preds == target == 1).sum()
-        FP = (preds != target == 0).sum()
-        TN = (preds == target == 0).sum()
-        FN = (preds != target == 1).sum()
+        TP = torch.logical_and(Y, X).sum() #(preds == target == 1).sum()
+        FP = torch.logical_not(torch.logical_and(torch.logical_not(Y), X)).sum()# (preds != target == 0).sum()
+        TN = torch.logical_not(torch.logical_and(Y, X)).sum() # (preds == target == 0).sum()
+        FN = torch.logical_and(torch.logical_not(Y), X).sum() # (preds != target == 1).sum()
         # Sensitivity
         sensitivity = TP/(TP+FN)
 
